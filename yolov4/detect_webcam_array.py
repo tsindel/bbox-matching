f20a0d23312460d10df9a7e15127ad31ad98c0fc
tsindel <84905798+tsindel@users.noreply.github.com>
2021-08-23 17:59:32 +0200

    moving gpu detection before initialization

--- yolov4/detect_webcam_array.py
+++ yolov4/detect_webcam_array.py
@@ -1,16 +1,15 @@
 import sys
 import numpy as np
 import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 import core.utils as utils
 import cv2
 from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
-
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 tf.compat.v1.app.flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
 tf.compat.v1.app.flags.DEFINE_string('weights', './checkpoints/yolov4-416', 'path to weights file')
